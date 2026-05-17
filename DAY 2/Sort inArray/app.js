2ff8bc58e844f3ab64b31d02a4bf6feb92e61e60
pankaj Hajra <pankuhajra07@gmail.com>
2026-05-18 00:16:15 +0530

    Sorting methode in Arrays

---
+++ DAY 2/Sort inArray/app.js
@@ -0,0 +1,13 @@
+let days = ["monday","sunday","wednesday","tuesday"];
+
+console.log(days.sort());
+
+
+let chars = ['b','d','e','a'];
+
+console.log(chars.sort());
+
+
+let num = [68,100,90,91,99];
+
+console.log(num.sort()); /// not sorted as expected
