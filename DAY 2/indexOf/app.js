46e2be11f1b4d496e5853ed3a134b0ea7942ce67
pankaj Hajra <pankuhajra07@gmail.com>
2026-05-17 22:14:35 +0530

    IndexOf methode

---
+++ DAY 2/indexOf/app.js
@@ -0,0 +1,9 @@
+let primary = ["Red","Yellow","Green"];
+let a = primary.indexOf("Yellow");
+let b = primary.indexOf("blue"); // blue doesn't exist
+let c = primary.indexOf("yellow");
+
+
+console.log(a); //1
+console.log(b); //-1
+console.log(c); //-1
---
+++ DAY 2/indexOf/app1.js
@@ -0,0 +1,6 @@
+let car = ['audi','bmw','RR','Merc'];
+
+let a = car.indexOf("audi");
+let b = car.indexOf('bmw')
+console.log(a);
+console.log(b);
