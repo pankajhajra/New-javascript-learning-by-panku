63e2e45ee999921cd6cd5e2b463e1e4cc427bc18
pankaj Hajra <pankuhajra07@gmail.com>
2026-05-25 23:01:27 +0530

    Array methodes

---
+++ DAY 2/Arrays/Contate/app.js
@@ -0,0 +1,6 @@
+let a = ["Red","Yellow","Green"];
+let b = ["Blue","Pink","Red"];
+
+let c = a.concat(a);
+
+console.log(c)
---
+++ DAY 2/Arrays/Reverse/app.js
@@ -0,0 +1,5 @@
+let a = ["Red","Black","Maroon"];
+
+let c = a.reverse();
+
+console.log(c);
